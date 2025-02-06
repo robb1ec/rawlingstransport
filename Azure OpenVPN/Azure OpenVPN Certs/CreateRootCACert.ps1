fcf88d4f0e8d96e4c5fc06e7fecf270b224749e4
RobbieC <10465564+robb1ec@users.noreply.github.com>
2025-02-06 20:01:23 +0000

    Update CreateRootCACert.ps1

--- Azure OpenVPN/Azure OpenVPN Certs/CreateRootCACert.ps1
+++ Azure OpenVPN/Azure OpenVPN Certs/CreateRootCACert.ps1
@@ -1,3 +1,23 @@
+# root certificate
+
+ $params = @{
+    Type = 'Custom'
+    Subject = 'CN=wan.91n4p7vmsaugl2snqu0o0g3pd.vpn.azure.com'
+    KeySpec = 'Signature'
+    KeyExportPolicy = 'Exportable'
+    KeyUsage = 'CertSign'
+    KeyUsageProperty = 'Sign'
+    KeyLength = 2048
+    HashAlgorithm = 'sha256'
+    NotAfter = (Get-Date).AddMonths(120)
+    CertStoreLocation = 'Cert:\CurrentUser\My'
+}
+$cert = New-SelfSignedCertificate @params 
+
+exit 0
+
+
+
 $params = @{
     Type = 'Custom'
     Subject = 'CN=P2SRootCert'
